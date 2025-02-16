6d346b7448a91723ccccbbf37b99b61b6d001236
FoxRLY <nevik0016@gmail.com>
2025-02-16 17:53:41 +0300

    Пара правок

--- app/app.cpp
+++ app/app.cpp
@@ -1,9 +1,28 @@
 #include <logger/simple.h>
 #include <fmt/format.h>
+#include <thread>
 
 int main() {
     auto log = logger::SimpleLogger(logger::LogLevel::INFO);
-    for (int i = 0; i < 100; i++) {
-        log.info(fmt::format("Hello: {}", i));
-    }
+
+    auto printer = [&](int number) {
+        for (int i = 0; i < 100; i++) {
+            log.info(fmt::format("Hello {}: {}", number, i));
+        }
+    };
+
+    auto thread_one = std::thread(printer, 1);
+    auto thread_two = std::thread(printer, 2);
+    auto thread_three = std::thread(printer, 3);
+    auto thread_four = std::thread(printer, 4);
+    auto thread_five = std::thread(printer, 5);
+
+    thread_one.join();
+    thread_two.join();
+    thread_three.join();
+    thread_four.join();
+    thread_five.join();
+
+    log.info("END");
+
 }
